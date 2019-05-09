37ee7842bb60244685b9f11078ef4da7b0df4ad9
Michael Cole <M_Cole84@hotmail.com>
2019-05-09 16:19:02 -0400

    Added Api controller methods

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -16,5 +16,47 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var result = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+            if(result is null)
+            {
+                return NotFound();
+            }
+            result.Satellites = _context.CelestialObjects.Where(s => s.OrbitedObjectId == result.Id).ToList();
+
+            return Ok(result);
+
+        }
+
+        [HttpGet("name:string")]
+        public IActionResult GetByName(string name)
+        {
+            var result = _context.CelestialObjects.Where(c => c.Name == name);
+            if(result is null)
+            {
+                return NotFound();
+            }
+            foreach(var s in result)
+            {
+                s.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == s.Id).ToList();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _context.CelestialObjects.ToList();
+            foreach (var s in result)
+            {
+                s.Satellites = _context.CelestialObjects.Where(c => c.OrbitedObjectId == s.Id).ToList();
+            }
+
+            return Ok(result);
+        }
     }
 }
